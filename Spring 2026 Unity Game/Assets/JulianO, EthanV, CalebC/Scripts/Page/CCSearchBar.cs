673eaf62f427e585247092cd6dd9b5905569239f
ctcope228 <ctcope228@gmail.com>
2026-02-11 20:00:25 -0500

    Interactable base class
    Inherit from CCInteractableBase when creating interactable objects, implement Interact function. Tested: 0 errors.

---
+++ Spring 2026 Unity Game/Assets/JulianO, EthanV, CalebC/Scripts/Page/CCSearchBar.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class CCSearchBar : CCInteractableBase
+{
+    protected override void Interact()
+    {
+        Debug.Log("Search bar interact");
+    }
+}
