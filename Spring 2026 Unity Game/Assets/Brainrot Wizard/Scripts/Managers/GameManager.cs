bd4d0aed43d03456175d799c65097d1fc2ff743f
ChickenGamer <chickencodes@gmail.com>
2026-02-13 12:26:18 -0500

    Contains a basis for the economy and game manager
    Has the basic functions needed for the game at the moment. No errors.

---
+++ Spring 2026 Unity Game/Assets/Brainrot Wizard/Scripts/Managers/GameManager.cs
@@ -0,0 +1,113 @@
+using System;
+using UnityEngine;
+
+public enum GameState 
+{
+    None,
+    
+    /// <summary>
+    /// The player is taking on contracts, earning money, and selling stock to past clients.
+    /// </summary>
+    ContractWork,
+
+    /// <summary>
+    /// The player purchases brain-rot loot boxes.
+    /// </summary>
+    Shopping,
+    
+    /// <summary>
+    /// The player is actively combining brain‑rots to create new ones.
+    /// </summary>
+    Crafting
+}
+
+[RequireComponent(typeof(EconomyManager))]
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// Gets the current state that the game is in.
+    /// </summary>
+    public GameState CurrentGameState { get; private set; } = GameState.None;
+    
+    /// <summary>
+    /// Triggers when game state is changed, and reports back the new state.
+    /// </summary>
+    public Action<GameState> onGameStateChange;
+
+    public EconomyManager EconomyManager {get; private set;}
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject); 
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(gameObject); 
+    }
+
+    private void Start()
+    {
+        if (EconomyManager == null)
+            EconomyManager = GetComponent<EconomyManager>();
+    }
+
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("GameManager not found");
+            }
+            return _instance;
+        }
+    }
+
+    /// <summary>
+    /// Switches to the next game state. 
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when a invalid state is switched to.</exception>
+    public void NextState()
+    {
+        switch (CurrentGameState)
+        {
+            case GameState.None:
+                break;
+            case GameState.ContractWork:
+                CurrentGameState = GameState.Shopping;
+                ShoppingState();
+                break;
+            case GameState.Shopping:
+                CurrentGameState = GameState.Crafting;
+                CraftingState();
+                break;
+            case GameState.Crafting:
+                CurrentGameState = GameState.ContractWork;
+                ContractWorkState();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+        onGameStateChange.Invoke(CurrentGameState);
+    }
+    // TODO: Fill in with crafting state logic & operations
+    private void CraftingState()
+    {
+        
+    }
+    // TODO: Fill in with shopping state logic & operations
+    private void ShoppingState()
+    {
+        
+    }
+    // TODO: Fill in with contract work state logic & operations
+    private void ContractWorkState()
+    {
+        
+    }
+}
