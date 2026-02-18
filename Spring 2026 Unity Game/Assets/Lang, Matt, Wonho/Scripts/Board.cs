e89394e93189590e4c48c1ecd60e327fdb33d8bc
langchenrocket <lang.chen.rocket@gmail.com>
2026-02-18 18:49:50 -0500

    Converted code into method
    No errors

--- Spring 2026 Unity Game/Assets/Lang, Matt, Wonho/Scripts/Board.cs
+++ Spring 2026 Unity Game/Assets/Lang, Matt, Wonho/Scripts/Board.cs
@@ -3,11 +3,7 @@ using UnityEngine;
 public class Board : MonoBehaviour
 {
     public GridGeneration gridGenerationScript;
-
     private Row[] rows;
-
-    //private int rowIndex = 0;
-    //private int columnIndex = 0;
 
     private void Awake()
     {
@@ -15,26 +11,23 @@ public class Board : MonoBehaviour
     }
 
     private bool test = true;
-    private float timer = 0f;
 
     private void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= 2f)
+        if (test == true)
         {
-            if (test == true)
+            Invoke("DrawGrid", 0.5f);
+            test = false;
+        }
+    }
+
+    void DrawGrid()
+    {
+        for (int i = 0; i < 13; i++)
+        {
+            for (int j = 0; j < 13; j++)
             {
-                //Debug.Log("This is: " + gridGenerationScript.grid[0, 0]);
-
-                for (int i = 0; i < 13; i++)
-                {
-                    for (int j = 0; j < 13; j++)
-                    {
-                        rows[i].tiles[j].SetLetter(gridGenerationScript.grid[i, j]);
-                    }
-                }
-
-                test = false;
+                rows[i].tiles[j].SetLetter(gridGenerationScript.grid[i, j]);
             }
         }
     }
