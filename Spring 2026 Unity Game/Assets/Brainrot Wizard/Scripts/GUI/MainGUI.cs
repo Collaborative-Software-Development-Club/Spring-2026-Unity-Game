be906b1d6cf8e52d3d48d1e42eaf37c4d8469347
Vejou <65826623+vejou@users.noreply.github.com>
2026-02-25 19:53:01 -0500

    MainGUI setup with events
    No errors

---
+++ Spring 2026 Unity Game/Assets/Brainrot Wizard/Scripts/GUI/MainGUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+
+public class MainGUI : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI MoneyText;
+    public TMPro.TextMeshProUGUI ContractText;
+    public TMPro.TextMeshProUGUI StateText;
+    public TMPro.TextMeshProUGUI TurnText;
+
+    public string Money = "Monie: ";
+    public string Contract = "Current contract: ";
+    public string State = "GameState: ";
+    public string Turn = "Turn: ";
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Spring 2026 Unity Game/Assets/Brainrot Wizard/Scripts/Managers/GUIManager.cs
+++ Spring 2026 Unity Game/Assets/Brainrot Wizard/Scripts/Managers/GUIManager.cs
@@ -7,14 +7,19 @@ public class GUIManager : MonoBehaviour
     private GameManager gM;
     public Canvas canvas;
 
-    [SerializeField]
-    public MoneyGUI moneyGUI;
+    public GameObject mainObject;
+    public GameObject MainGUIPrefab;
+    public MainGUI main;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gM = GameManager.Instance;
         gM.EconomyManager.onMoneyChanged += UpdateMoneyGUI;
+        gM.onTurnChange += UpdateTurnGUI;
+
+        mainObject = GameObject.Instantiate(MainGUIPrefab);
+        main = mainObject.GetComponent<MainGUI>();
     }
 
     // Update is called once per frame
@@ -23,7 +28,16 @@ public class GUIManager : MonoBehaviour
         
     }
 
-    void UpdateMoneyGUI(double data) {
-
+    public void UpdateMoneyGUI(double data) {
+        main.MoneyText.text = main.Money + data;
+    }
+    public void UpdateContractGUI(double data) {
+        main.ContractText.text = main.Contract + data;
+    }
+    public void UpdateStateGUI(double data) {
+        main.StateText.text = main.State + data;
+    }
+    public void UpdateTurnGUI(int data) {
+        main.TurnText.text = main.Turn + data;
     }
 }
