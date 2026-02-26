c7cd155fd5763b7d2c5be8d8e2d16de4202cf15d
ChickenGamer <chickencodes@gmail.com>
2026-02-25 19:53:51 -0500

    Player Inventory
    Probably no errors. Adds basic functionality for inventory.

---
+++ Spring 2026 Unity Game/Assets/Brainrot Wizard/Scripts/Inventory/PlayerInventory.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInventory : MonoBehaviour
+{
+   private Inventory inventory = new(9);
+   [SerializeField] private PlayerInventoryUI playerInventoryUI;
+
+   private int currentSlot = 0;
+   
+   public void OnSlotClicked(InputAction.CallbackContext context)
+   { 
+       if (!context.started) return;
+
+       var slotIndex = int.Parse(context.control.name) - 1;
+        
+       if (slotIndex < 0 || slotIndex >= inventory.Length + 1)
+       {
+           Debug.LogWarning("Invalid slot index.");
+           return;
+       }
+
+       playerInventoryUI.HighlightInventorySlot(slotIndex);
+   }
+}
