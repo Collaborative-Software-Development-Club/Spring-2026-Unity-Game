8c51a35325a10ff5597ab4d0498e18c2816d5de1
Irene <suh.253@buckeyemail.osu.edu>
2026-03-04 17:03:36 -0500

    Changed Reset Option
    Added a press R to reset option and removed reset prefab.

--- Spring 2026 Unity Game/Assets/Anshuman, Ardelia, Irene/Scripts/ResetSceneDamp.cs
+++ Spring 2026 Unity Game/Assets/Anshuman, Ardelia, Irene/Scripts/ResetSceneDamp.cs
@@ -3,57 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class ResetSceneDamp : MonoBehaviour
 {
-    [Header("References")]
-    [SerializeField] private GameObject interactionPrompt;
-    [SerializeField] private InputReader inputReader;
-    private bool _isPlayerNearby = false;
-
-    private void Awake()
+    void Update()
     {
-        // Ensure everything is hidden at the start
-        if (interactionPrompt != null) interactionPrompt.SetActive(false);
-    }
-    private void OnEnable()
-    {
-        if (inputReader != null) inputReader.InteractEvent += HandleInteraction;
-    }
-
-    private void OnDisable()
-    {
-        if (inputReader != null) inputReader.InteractEvent -= HandleInteraction;
-    }
-
-    private void HandleInteraction()
-    {
-        if (_isPlayerNearby)
+        if (Input.GetKeyDown(KeyCode.R))
         {
             ResetLevel();
         }
     }
+
+
     void ResetLevel()
     {
         PlayerInv.water = 0;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("Player"))
-            {
-                _isPlayerNearby = true;
-                if (interactionPrompt != null)
-                {
-                    interactionPrompt.SetActive(true);
-                }
-            }
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            if (other.CompareTag("Player"))
-            {
-                _isPlayerNearby = false;
-                if (interactionPrompt != null) interactionPrompt.SetActive(false);
-            }
-        }
 }
