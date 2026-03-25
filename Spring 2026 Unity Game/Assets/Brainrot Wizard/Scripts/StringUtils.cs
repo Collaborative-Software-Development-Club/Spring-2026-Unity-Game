743c2efa3967ca7bace042aaf7fccc79200e56c5
ChickenGamer <chickencodes@gmail.com>
2026-03-25 19:38:57 -0400

    Working Tooltips
    Hovering over a item in the inventory gives various info. No errors

---
+++ Spring 2026 Unity Game/Assets/Brainrot Wizard/Scripts/StringUtils.cs
@@ -0,0 +1,44 @@
+using System;
+
+public static class StringUtils
+{
+    private static readonly string[] suffixes = { "", "K", "M", "B", "T" };
+
+    public static string AbbreviateNumber(int number)
+    {
+        return AbbreviateNumber((double)number);
+    }
+
+    public static string AbbreviateNumber(double number)
+    {
+        double value = number;
+        int index = 0;
+
+        while (value >= 1000 && index < suffixes.Length - 1)
+        {
+            value /= 1000d;
+            index++;
+        }
+
+        string formatted = (value % 1 == 0)
+            ? ((long)value).ToString() 
+            : value.ToString("0.##");        
+
+        return formatted + suffixes[index];
+    }
+
+    public static string PlaceSeparators(string stringToSeparate, string separator = " ")
+    {
+        var temp = "";
+        
+        for (var i = 0; i < stringToSeparate.Length; i++)
+        {
+            if (i + 1 < stringToSeparate.Length && Char.IsUpper(stringToSeparate[i + 1]))
+                temp += separator;
+                
+            temp +=  stringToSeparate[i];
+        }
+
+        return temp;
+    }
+}
