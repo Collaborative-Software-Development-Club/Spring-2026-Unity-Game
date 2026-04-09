454a868e7e228339c77184c55506ee058162e73e
Matt Wang <yxmatt.wang@gmail.com>
2026-04-08 21:31:01 -0400

    Spell prints random letter now
    no errors

--- Spring 2026 Unity Game/Assets/Lang, Matt, Wonho/Scripts/LetterSpell.cs
+++ Spring 2026 Unity Game/Assets/Lang, Matt, Wonho/Scripts/LetterSpell.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class LetterSpell : SpellBehavior
 {
+    private string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     public override void CastSpell(Transform playerTransform)
     {
-        Debug.Log("A");
+        char letter = RandomLetter();
+        Debug.Log(letter);
         // Logic to spawn a fireball projectile, play sounds, etc.
+    }
+    char RandomLetter()
+    {
+        int letter = Random.Range(0, 26);
+        return letters[letter];
     }
 }
