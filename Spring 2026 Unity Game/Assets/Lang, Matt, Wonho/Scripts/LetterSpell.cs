22da15f75efc7ee0addfe2eb3770eedf522b1ea5
langchenrocket <lang.chen.rocket@gmail.com>
2026-04-15 19:53:53 -0400

    Started text spell
    There is a null error but shouldn't affect it, Trevor approved the push

--- Spring 2026 Unity Game/Assets/Lang, Matt, Wonho/Scripts/LetterSpell.cs
+++ Spring 2026 Unity Game/Assets/Lang, Matt, Wonho/Scripts/LetterSpell.cs
@@ -1,13 +1,43 @@
 using UnityEngine;
 
+using TMPro;
+using UnityEngine.UI;
+
 public class LetterSpell : SpellBehavior
 {
     private string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    public TextMeshPro textDisplay; // Use TextMeshPro for 3D
+
+    void Awake()
+    {
+        // Force the script to find the text component on THIS object
+        //textDisplay = GetComponent<TextMeshProUGUI>();
+
+        // This will tell you EXACTLY if it failed to find it
+        if (textDisplay == null)
+        {
+            Debug.LogError($"TMP Component missing on {gameObject.name}!", this);
+        }
+    }
+
+    public void ChangeMyText(char letter)
+    {
+        //textDisplay.enabled = true;
+        if (textDisplay != null)
+        {
+            textDisplay.text = "" + letter;
+        }
+        else
+        {
+            Debug.Log("Darn");
+        }
+    }
     public override void CastSpell(Transform playerTransform)
     {
         char letter = RandomLetter();
         Debug.Log(letter);
+        ChangeMyText(letter);
         // Logic to spawn a fireball projectile, play sounds, etc.
     }
     char RandomLetter()
