b6057df5a3efbe0103c48dfeee1b8c5a4b24b389
AnonymousPseudonymous <oconerj41@gmail.com>
2026-04-19 14:18:20 -0400

    Key Hiding
    Implemented functionality of keys being hidden and uncollectable until the popup is closed.
    
    nothing broke 👍

--- Spring 2026 Unity Game/Assets/JulianO, EthanV, CalebC/Scripts/Keyboard/JEC_KeyCollectable.cs
+++ Spring 2026 Unity Game/Assets/JulianO, EthanV, CalebC/Scripts/Keyboard/JEC_KeyCollectable.cs
@@ -1,14 +1,24 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class JEC_KeyCollectable : MonoBehaviour
 {
     [SerializeField] private JEC_Key keyData;
+    [SerializeField] private Boolean isHidden;
 
     private string character;
 
     private void Start()
     {
+
+        JEC_Events.OnPopupClosed.AddListener(Appear);
+        // hide the key if is marked hidden by popup
+        if (isHidden)
+        {
+            gameObject.SetActive(false);
+        }
+
         if (keyData != null)
         {
             character = keyData.character;
@@ -26,5 +36,10 @@ public class JEC_KeyCollectable : MonoBehaviour
         JEC_Events.OnPickupKey.Invoke(character);
 
         Destroy(gameObject);
+    }
+
+    void Appear()
+    {
+        gameObject.SetActive(true);
     }
 }
