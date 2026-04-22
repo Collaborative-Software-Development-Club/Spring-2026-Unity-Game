0b62f515b74f2eda3110236daa61ee9cebe31a37
csandcastle <csandcastle119@gmail.com>
2026-04-21 22:08:03 -0400

    Added arrow visual

--- Spring 2026 Unity Game/Assets/Sanders, Tristan, Cameron/Scripts/Essence System/EssenceMover.cs
+++ Spring 2026 Unity Game/Assets/Sanders, Tristan, Cameron/Scripts/Essence System/EssenceMover.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EssenceMover : MonoBehaviour
@@ -9,11 +10,17 @@ public class EssenceMover : MonoBehaviour
     [SerializeField][Range(0f, 1f)] private float windBoostVolume = 1f;
     [SerializeField][Min(0f)] private float windSoundCooldown = 0.1f;
 
+    [SerializeField] GameObject arrow;
+    [SerializeField] GameObject cross;
+
     private float lastWindSoundTime = float.NegativeInfinity;
+
+    //private Dictionary<Collider2D, float> objectsTimeInWind;
 
     private void Awake()
     {
         EnsureWindAudioSource();
+        //objectsTimeInWind = new();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,12 +28,16 @@ public class EssenceMover : MonoBehaviour
         if (collision.CompareTag("Essence"))
         {
             PlayWindBoostAudio();
+            //objectsTimeInWind.Add(collision, 0);
         }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        ApplyForce(collision);
+        if(collision.CompareTag("Essence"))
+        {
+            ApplyForce(collision);
+        }
     }
 
     private void ApplyForce(Collider2D other)
@@ -38,6 +49,7 @@ public class EssenceMover : MonoBehaviour
 
         Vector2 forceDir = -transform.up;
         other.attachedRigidbody.AddForce(forceDir * force, ForceMode2D.Force);
+
     }
 
     private void EnsureWindAudioSource()
@@ -77,5 +89,24 @@ public class EssenceMover : MonoBehaviour
 
         windAudioSource.PlayOneShot(windBoostClip, windBoostVolume);
         lastWindSoundTime = Time.time;
+    }
+
+    /// <summary>
+    /// Displays an arrow if spell can be placed
+    /// Display an X if spell can not be placed
+    /// </summary>
+    /// <param name="isValid"></param>
+    public void DisplayPlaceability(bool isValid)
+    {
+        if (isValid)
+        {
+            arrow.SetActive(true);
+            cross.SetActive(false);
+        }
+        else
+        {
+            arrow.SetActive(false);
+            cross.SetActive(true);
+        }
     }
 }
