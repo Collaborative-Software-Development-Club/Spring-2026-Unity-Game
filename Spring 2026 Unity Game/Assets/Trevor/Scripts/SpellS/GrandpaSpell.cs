2a0bd71b46927970ce2333da1dae72e575108d14
ethanVincent24 <ethanvincent244@gmail.com>
2026-04-21 21:54:11 -0400

    GRANDPA SPELL
    Summon Up to 8 WONDROUS grandpas at a time upon beating the world wide wizard level.
    
    No errors

--- Spring 2026 Unity Game/Assets/Trevor/Scripts/SpellS/GrandpaSpell.cs
+++ Spring 2026 Unity Game/Assets/Trevor/Scripts/SpellS/GrandpaSpell.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GrandpaSpell : SpellBehavior
@@ -5,6 +8,11 @@ public class GrandpaSpell : SpellBehavior
     [SerializeField] private GameObject grandpa;
     [SerializeField] private Vector3 summonLocation;
     [SerializeField] private Quaternion summonRotation;
+    [SerializeField] private int grandpaLimit = 8;
+
+    private int grandpaCount = 0;
+    private int destroyedCount = 0;
+    private List<GameObject> Grandpas = new List<GameObject>();
     public override void CastSpell(Transform playerTransform)
     {
         SummonGrandpa(playerTransform);
@@ -12,6 +20,14 @@ public class GrandpaSpell : SpellBehavior
 
     public void SummonGrandpa(Transform location)
     {
-        Instantiate(grandpa, location.position, summonRotation);
+        if (grandpaCount >= grandpaLimit)
+        {
+            Destroy(Grandpas[destroyedCount]);
+            destroyedCount++;
+            grandpaCount--;
+        }
+
+        Grandpas.Add(Instantiate(grandpa, location.position, summonRotation));
+        grandpaCount++;
     }
 }
